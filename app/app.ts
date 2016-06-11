e1af02ae4fb00372ba43d91f5ccc05d853ae2e17
aaronksaunders <aaron@clearlyinnovative.com>
2016-06-11 16:26:58 -0400

    initial checkin

---
+++ app/app.ts
@@ -0,0 +1,25 @@
+//import 'es6-shim';
+import {Component} from '@angular/core';
+import {Platform, ionicBootstrap} from 'ionic-angular';
+import {StatusBar} from 'ionic-native';
+import {HomePage} from './pages/home/home';
+
+
+@Component({
+  template: '<ion-nav [root]="rootPage"></ion-nav>'
+})
+export class MyApp {
+  rootPage: any
+
+  constructor(platform: Platform) {
+    platform.ready().then(() => {
+      // Okay, so the platform is ready and our plugins are available.
+      // Here you can do any higher level native things you might need.
+      StatusBar.styleDefault();
+
+      this.rootPage  = HomePage;
+    });
+  }
+}
+
+ionicBootstrap(MyApp);
---
+++ app/pages/home/home.ts
@@ -0,0 +1,59 @@
+import {Component} from "@angular/core";
+import {NavController, Platform} from 'ionic-angular';
+import {GoogleMap, GoogleMapsEvent, GoogleMapsLatLng, GoogleMapsMarkerOptions} from 'ionic-native';
+import {Observable} from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+
+//const mapConfig: any;
+
+@Component({
+  templateUrl: 'build/pages/home/home.html'
+})
+export class HomePage {
+  private map: GoogleMap;
+
+  constructor(private _navController: NavController, private platform: Platform) {
+    this.platform.ready().then(() => this.onPlatformReady());
+  }
+
+
+  private onPlatformReady(): void {
+    this.map = new GoogleMap('map_canvas');
+
+    GoogleMap.isAvailable().then(() => {
+      this.map.on(GoogleMapsEvent.MAP_READY).subscribe(
+        () => this.onMapReady(),
+        () => alert("Error: onMapReady")
+      );
+
+      this.map.on(GoogleMapsEvent.MAP_READY).subscribe(
+        (data: any) => {
+          alert("GoogleMap.onMapReady(): ");
+        },
+        () => alert("Error: GoogleMapsEvent.MAP_READY")
+      );
+
+      this.map.one(GoogleMapsEvent.MAP_READY).then((data: any) => {
+        alert("GoogleMap.onMapReady(): " + JSON.stringify(data));
+      });
+    });
+
+
+
+    let myPosition = new GoogleMapsLatLng('38.9072', '-77.0369');
+    console.log("My position is", myPosition);
+
+    this.map.animateCamera({ target: myPosition, zoom: 10 });
+
+  }
+
+  private onMapReady(): void {
+    alert('Map ready');
+    //this.map.setOptions(mapConfig);
+  }
+  /*
+    pushPage(){
+      this._navController.push(SomeImportedPage, { userId: "12345"});
+    }
+  */
+}
