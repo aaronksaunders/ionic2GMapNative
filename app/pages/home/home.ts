58a491f825f6607c86001cde05cea2dab5c153da
aaronksaunders <aaron@clearlyinnovative.com>
2016-08-24 02:07:05 -0400

    updated to latest release

--- app/pages/home/home.ts
+++ app/pages/home/home.ts
@@ -1,4 +1,4 @@
-import {Component} from "@angular/core";
+import {Component, NgZone} from "@angular/core";
 import {NavController, Platform} from 'ionic-angular';
 import {GoogleMap, GoogleMapsEvent, GoogleMapsLatLng, GoogleMapsMarkerOptions} from 'ionic-native';
 import {Observable} from 'rxjs/Observable';
@@ -12,17 +12,22 @@ import 'rxjs/add/operator/map';
 export class HomePage {
   private map: GoogleMap;
 
-  constructor(private _navController: NavController, private platform: Platform) {
+  constructor(private _navController: NavController,
+    private platform: Platform,
+    private _zone: NgZone) {
     this.platform.ready().then(() => this.onPlatformReady());
   }
 
 
   private onPlatformReady(): void {
 
+  }
+
+  ngAfterViewInit() {
     GoogleMap.isAvailable().then(() => {
-      
+
       this.map = new GoogleMap('map_canvas');
-          
+
       // this.map.on(GoogleMapsEvent.MAP_READY).subscribe(
       //   () => this.onMapReady(),
       //   () => alert("Error: onMapReady")
@@ -38,16 +43,14 @@ export class HomePage {
       this.map.one(GoogleMapsEvent.MAP_READY).then((data: any) => {
         alert("GoogleMap.onMapReady(): " + JSON.stringify(data));
 
-        let myPosition = new GoogleMapsLatLng(38.9072, -77.0369);
-        console.log("My position is", myPosition);
+        this._zone.run(() => {
+          let myPosition = new GoogleMapsLatLng(38.9072, -77.0369);
+          console.log("My position is", myPosition);
+          this.map.animateCamera({ target: myPosition, zoom: 10 });
+        });
 
-        this.map.animateCamera({ target: myPosition, zoom: 10 });
       });
     });
-
-
-
-
   }
 
   private onMapReady(): void {
